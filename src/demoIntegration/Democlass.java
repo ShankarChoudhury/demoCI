e11b0d372572ebeeac637d60dd17f7fbf0799558
Shankar Choudhury <shan.choudhury@gmail.com>
2018-06-24 21:32:28 -0500

    Signed-off-by: Shankar Choudhury <shan.choudhury@gmail.com>

---
+++ src/demoIntegration/Democlass.java
@@ -0,0 +1,13 @@
+package demoIntegration;
+
+public class Democlass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a demo test to validate Travis Integration");
+	}
+
+}
